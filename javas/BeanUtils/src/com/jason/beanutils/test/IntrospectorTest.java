6bdb570a861ab4e06d23c0e54331ab330746b89e
lucasli <1906859953@qq.com>
2019-10-23 22:51:56 +0800

    jdk的bean内省技术，Introspector.getBeanInfo(class)

--- javas/BeanUtils/src/com/jason/beanutils/test/IntrospectorTest.java
+++ javas/BeanUtils/src/com/jason/beanutils/test/IntrospectorTest.java
@@ -1,11 +1,15 @@
 package com.jason.beanutils.test;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
+
+import com.jason.beanutils.Person;
 
 public class IntrospectorTest {
 	
@@ -28,5 +32,27 @@ public class IntrospectorTest {
 		
 		System.out.println(user.getUsername());
 		System.out.println(user.getPassword());
+	}
+	
+	@Test
+	public void demo() throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Person person=new Person();
+		System.out.println(person);
+		BeanInfo info=Introspector.getBeanInfo(Person.class);
+		//获取属性描述
+		PropertyDescriptor[] propertyDescriptors = info.getPropertyDescriptors();
+		//遍历输出
+		System.out.println("----------------");
+		for (PropertyDescriptor pd : propertyDescriptors) {
+			if(!"class".equals(pd.getName())){
+				if("name".equals(pd.getName())){
+					//获取写方法
+					Method writeMethod = pd.getWriteMethod();
+					writeMethod.invoke(person, "xiaoming");
+				}
+			}
+		}
+		
+		System.out.println(person);
 	}
 }
